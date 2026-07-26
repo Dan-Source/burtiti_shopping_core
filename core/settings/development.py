be557aa5dd2413bf569652d37433e1e5e43b4cb5
Dan-Source <dan.dluis.dl@gmail.com>
2026-07-26 17:28:49 -0300

    Fix docker compose and frontend Dockerfile

--- core/settings/development.py
+++ core/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 # Development-specific settings
 DEBUG = True
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "web"]
 OSCARAPI_ENABLE_REGISTRATION = True
 
 CORS_ALLOWED_ORIGINS = CORS_ALLOWED_ORIGINS + [
